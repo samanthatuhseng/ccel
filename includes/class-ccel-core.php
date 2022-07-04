95b1da1534f71b336126f95cc9184174c0174d20
kelvin <kelvin.xu@ubc.ca>
2022-07-04 10:32:21 -0700

    Enqueued filter assets including JavaScript and CSS to the homepage.

--- includes/class-ccel-core.php
+++ includes/class-ccel-core.php
@@ -19,7 +19,29 @@ class CCELCore {
 	 * @since    1.0.0
 	 */
 	public function __construct() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_filter_assets' ) );
+	}
 
+	public function enqueue_filter_assets() {
+		if( ! is_front_page() ) {
+			return;
+		}
+
+		wp_enqueue_script(
+			'ubc-ccel-filter-script',
+			UBC_CCEL_PLUGIN_URL . 'dist/filter.js',
+			array(),
+			filemtime( UBC_CCEL_PLUGIN_DIR . 'dist/filter.js' ),
+			true
+		);
+
+		wp_register_style(
+			'ubc-h5p-ownership-transfer-css',
+			UBC_CCEL_PLUGIN_URL . 'dist/filter.css',
+			array(),
+			filemtime( UBC_CCEL_PLUGIN_DIR . 'dist/filter.css' )
+		);
+		wp_enqueue_style( 'ubc-h5p-ownership-transfer-css' );
 	}
 
 }
